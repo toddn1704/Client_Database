71597188300c2648c269b94b581151f43d7fe1ca
Noelle Todd <decorationgirl@gmail.com>
2014-06-09 16:47:09 -0400

    Update cdbfunctions.py
    Removed some functions to be placed in another file. Added the get_age_breakdown function.

--- cdbfunctions.py
+++ cdbfunctions.py
@@ -3,10 +3,10 @@
 Developer: Noelle Todd
 Last Updated: June 9, 2014
 
-This module consists of functions which will be called by the user
-interface, in order to insert, delete, update, etc. data in the database.
-This module is still in its early testing stages; many more functions will
-be added or edited in the following weeks.
+This module consists of helper functions which will be called by another
+module that will called directly by the user interface. This module is 
+still in its early testing stages; many more functions will be added or
+edited in the following weeks.
 
 """
 
@@ -18,59 +18,8 @@ from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from cdbtabledef import Household, Person, Volunteer, Visit
 
-engine = create_engine('sqlite:///test_db.sqlite')
-session = sessionmaker()
-session.configure(bind=engine)
 
-base = declarative_base()
-
-s = session()
-
-def quit_session():
-	"""This function will close the session.
-	"""
-	s.close()
-	
-def cancel_changes():
-	"""This function will rollback transactions & close session.
-	"""
-	s.rollback()
-	s.close()
-	
-def new_client(firstname, lastname, dob, datejoined, street,
-				dateverified = None, phonenum = None, Apt = None,
-				City = 'Troy', State = 'NY', Zip = '12180'):
-	"""This function creates a new person, household, and first visit.
-	If the page viewed is for a new_client, then this connects to the SAVE
-	button in the interface.
-	
-	For input, the function takes lists of strings for firstname and lastname, 
-	a list of date objects for dob, a list of integers for phonenum, strings
-	for Apt, City, and State, and an integer for Zip. There is no return.
-	
-	"""
-	
-	#create new household
-	newhouse = insert_household(street, dateverified, Apt, City, State, Zip)
-	
-	#create new person for every household member
-	if type(firstname) == list:
-		membersum = len(firstname) #finds length of list of names
-		for i in range(0, membersum):
-			insert_person(firstname[i], lastname[i], dob[i], datejoined,
-						newhouse)
-	else:
-		insert_person(firstname, lastname, dob, datejoined, newhouse,
-					phonenum)
-
-	#Additions:
-	#	Can we make the input any simpler?
-	#	How do we deal with multiple phone numbers?
-	#	Create a new visit.
-	
-	s.commit()
-	
-	
+#Functions for inserts
 def insert_household(street, dateverified = None, Apt = None, City = 'Troy',
 					State = 'NY', Zip = '12180'):
 	"""This function creates a new row to hold a household's data. It returns
@@ -123,3 +72,28 @@ def age(dob):
 			return timey.year - dob.year
 		else:
 			return timey.year - dob.year - 1
+
+
+def get_age_breakdown(members):
+	"""This function will retrieve all the ages of the members, and return the
+	number of adults, seniors, children, and infants accordingly.
+	
+	"""	
+	infants = 0
+	children = 0
+	adults = 0
+	seniors = 0
+	
+	for member in members:
+		if member.age < 2:
+			infants = infants + 1
+		elif member.age >= 2 and member.age < 18:
+			children = children + 1
+		elif member.age >= 18 and member.age < 65:
+			adults = adults + 1
+		else:
+			seniors = seniors + 1
+			
+	agegroups = {'infants':infants, 'children':children, 'adults':adults,
+				'seniors':seniors}	
+	return agegroups
